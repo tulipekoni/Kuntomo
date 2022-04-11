cd188a395b1d1d20fb5821604dad5ec37cc96142
Joonas Konttila <joonkont@gmail.com>
2022-04-11 16:52:22 +0300

    added startReviews

---
+++ src/components/StarReview.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+import { device } from "../infrastructure/media-queries/device";
+export default function StarReview({ starCount, label }) {
+  return (
+    <Container>
+      <div>
+        {[...Array(starCount)].map(() => {
+          return <Star hide={false} />;
+        })}
+      </div>
+      <Label marginLeft={5 - starCount}>{label}</Label>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0;
+  flex-direction: column;
+  @media ${device.tablet} {
+    flex-direction: row;
+  }
+`;
+const Label = styled.p`
+  line-height: 0;
+  margin-left: 0;
+  @media ${device.tablet} {
+    margin-left: ${(props) => props.marginLeft * 22 + 8}px;
+  }
+`;
+
+const Star = ({ hide }) => {
+  return (
+    <svg
+      width='18'
+      height='17'
+      viewBox='0 0 18 17'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      style={{ margin: 2, paddingBottom: 4 }}
+    >
+      <path
+        d='M9.24002 1.22668L11.6708 6.15122L17.1067 6.94575L13.1734 10.7768L14.1016 16.1891L9.24002 13.6324L4.37842 16.1891L5.30669 10.7768L1.37335 6.94575L6.80922 6.15122L9.24002 1.22668Z'
+        fill={hide ? "fff" : "#8A6FFF"}
+        stroke={hide ? "fff" : "#8A6FFF"}
+        stroke-width='1.57333'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
+  );
+};
--- src/pages/asiakastarinat/AsiakastarinatHero.js
+++ src/pages/asiakastarinat/AsiakastarinatHero.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ImageSVG from "../../components/image-svg";
-
+import StarReview from "../../components/StarReview";
+import styled from "styled-components";
+import { device } from "../../infrastructure/media-queries/device";
 export default function () {
   return (
-    <div style={{ display: "flex", flexDirection: "row", padding: 64 }}>
+    <Container>
       <div>
         <h1>Palvelumme pyörivät sinun ehdoillasi!</h1>
         <p>
@@ -11,11 +13,33 @@ export default function () {
           Meillä on ainutlaatuinen suhde jokaiseen asiakkaaseemme. Liity sinäkin
           osaksi tyytyväistä asiakaskuntaamme!
         </p>
+        <StarContainer>
+          <StarReview starCount={5} label='Salilehti' />
+          <StarReview starCount={5} label='TrackRun magazine' />
+          <StarReview starCount={4} label='GymShark' />
+        </StarContainer>
       </div>
 
-      <div style={{ flex: 1 }}>
+      <ImageContainer>
         <ImageSVG i={1} />
-      </div>
-    </div>
+      </ImageContainer>
+    </Container>
   );
 }
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 64px;
+`;
+const ImageContainer = styled.div`
+  flex: 1;
+`;
+const StarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-self: center;
+  justify-content: space-around;
+  @media ${device.tablet} {
+    flex-direction: column;
+  }
+`;
