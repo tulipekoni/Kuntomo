7fb03b23ede8aff87e69502400741162e93b35b1
Joonas Konttila <joonkont@gmail.com>
2022-04-12 21:26:25 +0300

    added slider global component

---
+++ src/components/Slider.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Slider({ min, max, title, value, setValue, unit }) {
+  return (
+    <SliderContainer>
+      <Title>{title}</Title>
+      <input
+        type='range'
+        min={min}
+        max={max}
+        className='slider'
+        value={value}
+        onChange={(a) => {
+          setValue(a.target.value);
+          console.log(a.target.value);
+        }}
+      />
+      <ValueText>{value}</ValueText>
+      <UnitText>{unit}</UnitText>
+    </SliderContainer>
+  );
+}
+
+const SliderContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+`;
+const Title = styled.h2`
+  margin-bottom: 48px;
+`;
+const ValueText = styled.h2`
+  margin: 0;
+  margin-top: 32px;
+`;
+const UnitText = styled.h2`
+  margin: 0;
+`;
--- src/pages/hintalaskuri/Hintalaskuri.js
+++ src/pages/hintalaskuri/Hintalaskuri.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PageContainer from "../../components/PageContainer";
 import HintalaskuriHero from "./HintalaskuriHero";
+import Laskuri from "./Laskuri";
 
 export default function Hintalaskuri() {
   return (
     <PageContainer>
       <HintalaskuriHero />
+      <Laskuri />
     </PageContainer>
   );
 }
